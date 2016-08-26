ec319071ecd3b50b0a3bf13ead85681735ca2f30
Gulshan <test.gulshan@gmail.com>
2016-08-27 01:33:38 +0530

    Added registration process.

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  'url' : 'node-authentication'
-}
+  'url' : 'mongodb://localhost/node-auth' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+};
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// set up ======================================================================
+// get all the tools we need
 var express  = require('express');
 var app      = express();
 var port     = process.env.PORT || 8080;
@@ -12,28 +14,27 @@ var session      = require('express-session');
 
 var configDB = require('./config/database.js');
 
-// configuration ================================
-// mongoose.connect(configDB.url); // connect to the database
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
 
-// require('./config/passport')(passport); // pass passport for configuration
+require('./config/passport')(passport); // pass passport for configuration
 
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser.json()); // get information from html forms
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser()); // get information from html forms
 
 app.set('view engine', 'ejs'); // set up ejs for templating
 
 // required for passport
-app.use(session({ secret: 'firstnodeauthenticationapp'})); // session secret
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
 app.use(passport.initialize());
-app.use(passport.session()); // persistent login session
+app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
-// routes =======================================
+// routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
-// launch =======================================
+// launch ======================================================================
 app.listen(port);
-console.log('Server is started at http://www.localhost:' + port);
+console.log('The magic happens on port ' + port);
